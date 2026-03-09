28ce66d149c6fdc93a869579ce4885d1b849abb7
ArmaanSingh <armaan.kharod@gmail.com>
2026-03-09 13:34:02 +0530

    Update Supabase client initialization with env variables

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
-mport { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://ptrccydgtbugtegxnfeu.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0cmNjeWRndGJ1Z3RlZ3huZmV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMjQ1ODEsImV4cCI6MjA4ODYwMDU4MX0.aeCbcTBdRaR3opBza_4fxt8nb4hMiEN3-eZUT60Vq04"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
